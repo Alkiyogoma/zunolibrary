188819a0e77bc06d77e21fd737d5274662d830ac
Albogast PC <albogasty@gmail.com>
2023-03-16 10:23:01 +0300

    added new files

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { Link } from '@inertiajs/vue3'
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    return pages[`./Pages/${name}.vue`]
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .component("Link", Link)
+      .mount(el)
+  },
+  progress: {
+    color: 'red',
+    showSpinner: true,
+  },
+})
